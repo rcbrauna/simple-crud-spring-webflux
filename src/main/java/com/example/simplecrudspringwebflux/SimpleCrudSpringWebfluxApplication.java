ef3241e651d170f6e16b0ae2d03e16a28e011c27
rcbrauna <rcbrauna@gmail.com>
2020-12-22 12:46:54 -0200

    init spring project

---
+++ src/main/java/com/example/simplecrudspringwebflux/SimpleCrudSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudspringwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudSpringWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplecrudspringwebflux/SimpleCrudSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudspringwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudSpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
